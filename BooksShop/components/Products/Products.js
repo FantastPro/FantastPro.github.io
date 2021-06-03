2079761eee39ea21d6a7006b6d0c0e5c352c2aed
Fantast <rybaku.turistu@gmail.com>
2021-06-03 20:56:47 +0300

    07

--- BooksShop/components/Products/Products.js
+++ BooksShop/components/Products/Products.js
@@ -1,14 +1,45 @@
 class Products {
+    constructor() {
+        this.classNameActive = 'products-element__btn_active';
+        this.labelAdd = 'Добавить в корзину';
+        this.labelRemove = 'Удалить из корзины';
+    }
+
+    handleSetLocationStorage(element, id) {
+        const { pushProduct, products } = localStorageUtil.putProducts(id);
+
+        if (pushProduct) {
+            element.classList.add(this.classNameActive);
+            element.innerHTML = this.labelRemove;
+        } else {
+            element.classList.remove(this.classNameActive);
+            element.innerHTML = this.labelAdd;
+        }
+    }
+
     render() {
+        const productsStore = localStorageUtil.getProducts();
         let htmlCatalog = '';
         CATALOG.forEach(({id, name, price, img, desc}) => {
+            let activeClass = '';
+            let activeText = '';
+
+            if (productsStore.indexOf(id) === -1) {
+                activeText = this.labelAdd;
+            } else {
+                activeClass = ' ' + this.classNameActive;
+                activeText = this.labelRemove;
+            }
+
             htmlCatalog += `
                 <li class="products-element">
                     <span class="products-element__name">${name}</span>
                     <img class="products-element__img" src="${img}">
                     <span class="products-element__desc">${desc}</span>
                     <span class="products-element__price">🛢️ ${price.toLocaleString()} RU</span>
-                    <button class="products-element__btn">Добавить в корзину</button>
+                    <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}')">
+                        ${activeText}
+                    </button>
                 </li>
             `
         });
