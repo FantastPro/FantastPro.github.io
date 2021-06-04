4c887ac59f9e75039868607fbb1685abdb9f0d07
Fantast <rybaku.turistu@gmail.com>
2021-06-04 16:18:14 +0300

    11

--- BooksShop/index.js
+++ BooksShop/index.js
@@ -7,15 +7,21 @@ function render() {
 
 }
 
+spinnerPage.render();
+
 let CATALOG = [];
 
 // "https://api.jsonbin.io/b/60ba18fa9fc30168f1c3634e/latest"
+// 'server/catalog.json'
 
-fetch('server/catalog.json')
+fetch("https://api.jsonbin.io/b/60ba18fa9fc30168f1c3634e/latest")
     .then(res => res.json())
     .then(body => {
         CATALOG = body;
+
+        spinnerPage.handleClear();
         render();
+
     })
     .catch(error => {
         console.log(error);
