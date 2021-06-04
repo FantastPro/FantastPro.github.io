b6da5910ede7ebdc662dfd3c47cf543671448e44
Fantast <rybaku.turistu@gmail.com>
2021-06-04 07:57:59 +0300

    08 Counter done

--- BooksShop/components/Header/Header.js
+++ BooksShop/components/Header/Header.js
@@ -1,10 +1,10 @@
 class Header {
 
-    render() {
+    render(count) {
         const html = `
             <div class="header-container">
                 <div class="header-counter">
-                    📚 77
+                    📚 ${count}
                 </div>
             </div>
         `;
@@ -14,4 +14,7 @@ class Header {
 }
 
 const headerPage = new Header();
-headerPage.render();
+
+const productsStore = localStorageUtil.getProducts();
+
+headerPage.render(productsStore.length);
