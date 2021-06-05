e96a6e557f44bf0ffe15aa3dd2986c4eeb1e38e6
Fantast <rybaku.turistu@gmail.com>
2021-06-05 20:43:43 +0300

    Done

--- BooksShop/index.js
+++ BooksShop/index.js
@@ -14,7 +14,7 @@ let CATALOG = [];
 // "https://api.jsonbin.io/b/60ba18fa9fc30168f1c3634e/latest"
 // 'server/catalog.json'
 
-fetch("https://api.jsonbin.io/b/60ba18fa9fc30168f1c3634e/latest")
+fetch("server/catalog.json")
     .then(res => res.json())
     .then(body => {
         CATALOG = body;
